bdb9f9696088cf8fe1ba742ef43a99a5651d1a31
shoaibzaraan96 <hsriaz32@gmail.com>
2026-05-07 16:41:22 +0500

    multer adjust

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -85,6 +85,6 @@ userSchema.methods.generateRefreshToken = function(){
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
-}
+} 
 
 export const User = mongoose.model("User", userSchema)
